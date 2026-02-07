f0cad5ff06e616ea747387dd6be2092d4f84cf37
serikzhan <ivanovichivanov51@gmail.com>
2026-02-07 11:11:49 +0500

    practice 2

---
+++ practice2/boolen/Boolean_Operators.py
@@ -0,0 +1,18 @@
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
---
+++ practice2/boolen/Booleans_as_Comparison_Results.py
@@ -0,0 +1,15 @@
+#==	Equal	x == y	
+#!=	Not equal	x != y	
+#>	Greater than	x > y	
+#<	Less than	x < y	
+#>=	Greater than or equal to	x >= y	
+#<=	Less than or equal to	x <= y
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
