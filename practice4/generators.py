e24a03bdf8f78bb2582c97251bf465720def0fba
serikzhan <ivanovichivanov51@gmail.com>
2026-02-28 00:33:18 +0500

    practice43

---
+++ practice4/generators.py
@@ -0,0 +1,69 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+  
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
+  
+def my_generator():
+ yield 1
+ yield 2
+ yield 3
+
+ for value in my_generator(): 
+    print(value)
+  
