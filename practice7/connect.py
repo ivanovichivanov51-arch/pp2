332506adec3ef4130c775a9088b03a6b49425ff5
serikzhan <ivanovichivanov51@gmail.com>
2026-04-09 22:47:37 +0500

    practic53

--- practice7/connect.py
+++ practice7/connect.py
@@ -1,33 +1,51 @@
 import psycopg2
-from config import load_config
 
-def connect():
-    """ PostgreSQL деректер қорына қосылу """
-    conn = None
-    try:
-        # Конфигурацияны database.ini файлынан оқу
-        params = load_config()
+try:
+    connection = psycopg2.connect(
+        user="postgres",
+        password="sekow102", 
+        host="127.0.0.1",
+        port="5432",
+        database="postgres"
+    )
 
-        # Серверге қосылу
-        print('PostgreSQL деректер қорына қосылудамыз...')
-        conn = psycopg2.connect(**params)
-		
-        # Курсор жасау
-        cur = conn.cursor()
-        
-        # Нұсқасын тексеру
-        cur.execute('SELECT version()')
-        db_version = cur.fetchone()
+    cursor = connection.cursor()
 
-        print(f'Қосылу сәтті аяқталды! \nнуска: {db_version}')
-       
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Қате шықты: {error}")
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Байланыс жабылды.')
+    # 1. Кесте құру
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50),
+            age INT
+        );
+    ''')
 
-if __name__ == '__main__':
-    connect()
+    # 2. МӘЛІМЕТ ҚОСУ (INSERT)
+    # Өзіңнің атыңды немесе басқа есімді жазып көр
+    user_name = "Serikzhan"
+    user_age = 20
+    
+    cursor.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (user_name, user_age))
+    
+    # Өзгерісті сақтау (МАҢЫЗДЫ!)
+    connection.commit()
+    print(f"Базаға {user_name} сәтті қосылды!")
+
+    # 3. МӘЛІМЕТТІ ОҚУ (SELECT)
+    cursor.execute("SELECT id, name, age FROM users;")
+    rows = cursor.fetchall()
+
+    print("\nБАЗАДАҒЫ БАРЛЫҚ ПАЙДАЛАНУШЫЛАР:")
+    print("-" * 40)
+    for row in rows:
+        print(f"ID: {row[0]} | Аты: {row[1]} | Жасы: {row[2]}")
+    print("-" * 40)
+
+except Exception as error:
+    print("Қате шықты:", error)
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Байланыс жабылды.")
